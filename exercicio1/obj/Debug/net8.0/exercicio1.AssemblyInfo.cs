fab567007e6f3b2f23fc43c422c123245ae8ec7b
Arthur Pereira Santana <arthurklotes1@gmail.com>
2024-08-13 21:51:14 -0300

    Update nem repository

--- exercicio1/obj/Debug/net8.0/exercicio1.AssemblyInfo.cs
+++ exercicio1/obj/Debug/net8.0/exercicio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679669cecba4edb17e9ba0b5d0da1fa3c04b3f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
