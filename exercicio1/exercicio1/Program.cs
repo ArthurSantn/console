679669cecba4edb17e9ba0b5d0da1fa3c04b3f9d
Arthur Pereira Santana <arthurklotes1@gmail.com>
2024-08-13 21:47:24 -0300

    Initial commit

---
+++ exercicio1/exercicio1/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("OLHA O CARRO DA PAMONHA");
+
+string nome = "Arthur Santana";
+    Console.WriteLine("Olá, " + nome);
+
+const double PI = 3.14;
+
+Console.WriteLine(PI);
+
+int x = 10;
+int y = 2;
+Console.WriteLine(x*y);
+
+
+int dinheiro = 120;
+int horas = 8;
+
+if (dinheiro / horas == 15)
+{
+    Console.WriteLine("Parece que você está granando rasoávelmente bem por hora trabalhada");
+} else
+{
+    Console.WriteLine("ESCRAVO KKKKKKKKKKKKKKKKKK");
+}
+
+int num = 4;
+
+switch (num)
+{
+    case 1: Console.WriteLine("Número 1");
+        break;
+    case 2: Console.WriteLine("Número 2");
+        break;
+    case 3: Console.WriteLine("Número 3");
+        break;
+    default: Console.WriteLine("Número maior que 3");
+        break ;
+}
+
+
+
+int x = 10;
+    while (x <= 20) 
+    {
+        Console.WriteLine(x);
+        x++;
+    }
+*/
+for (int y = 10; y >= 5; y--)
+{
+    Console.WriteLine(y);
+}
