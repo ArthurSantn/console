08934f1d3e59628fc64100fd862c8c8ae5cb7ee1
Arthur Pereira Santana <arthurklotes1@gmail.com>
2024-10-09 19:33:26 -0300

    delete arquives

--- exercicio1/obj/Debug/net8.0/exercicio1.AssemblyInfo.cs
+++ exercicio1/obj/Debug/net8.0/exercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf8846cae2e3a1cb6ef11d722039e2fdcd67e56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc7cbe23a608f2ebd1d5cd727f53cc0501c6a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
